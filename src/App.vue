8c4850fff095ec3a8286d7497c3cadeb7a3d6f29
Nubbyx <vaszko.ors@gmail.com>
2026-02-08 19:01:41 +0100

    swipe mechanism

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,51 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { useRouter, RouterView } from 'vue-router'
+import { ref, watch } from 'vue'
+import { useSwipe } from '@vueuse/core'
+
 import Navbar from "./components/Navbar.vue"
 import Icon from './components/Icon.vue';
 import Profile from './components/Profile.vue';
 
+const router = useRouter()
 
-// debug
+const swipeArea = ref(null)
+
+const pages = ['/orarend', '/jegyek', '/', '/hianyzasok', '/beallitasok']
+
+const { direction } = useSwipe(swipeArea, {
+  threshold: 80,
+})
+
+watch(direction, (dir) => {
+  const current = router.currentRoute.value.path
+  const index = pages.indexOf(current)
+
+  if (dir === 'left' && index < pages.length - 1) {
+    router.push(pages[index + 1])
+  }
+
+  if (dir === 'right' && index > 0) {
+    router.push(pages[index - 1])
+  }
+})
+
 </script>
 
 <template>
-  <Icon/>
-  <Profile />
-  <Navbar/>
-  <RouterView />
-  <!-- 
-  idea
-  Change view with side to side swiping?
-  -->
+  <div ref="swipeArea" class="app">
+
+    <Icon/>
+    <Profile />
+    <Navbar/>
+    <RouterView />
+
+  </div>
 </template>
 
 <style scoped>
-
+  .app {
+    min-height: 100vh;
+    touch-action: pan-y;
+  }
 </style>
